c509e33acb972d976dc58cee9283be89cd3754ad
Atanas Dimitrov <seishin90@yandex.ru>
2015-03-07 12:58:16 +0200

    Butterknife integration. Refactoring.

--- app/src/main/java/co/naughtyspirit/habits/MainActivity.java
+++ app/src/main/java/co/naughtyspirit/habits/MainActivity.java
@@ -1,20 +1,15 @@
 package co.naughtyspirit.habits;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.squareup.otto.Subscribe;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import co.naughtyspirit.habits.auth.AuthProviderFactory;
 import co.naughtyspirit.habits.bus.BusProvider;
 import co.naughtyspirit.habits.bus.events.users.GetUserStatsEvent;
@@ -28,31 +23,28 @@ import co.naughtyspirit.habits.views.transforms.ZoomOutPageTransformer;
  * *
  * * NaughtySpirit 2015
  */
-public class MainActivity extends FragmentActivity implements OnClickListener {
+public class MainActivity extends FragmentActivity {
 
     private static final String TAG = MainActivity.class.getName();
     
-    private Button logout;
-    private TextView expValue;
-    private ImageView expBarMask;
-    private float viewWidth;
-    private TextView hpValue;
-    private ImageView hpBarMask;
-    private ViewPager viewPager;
-    private RelativeLayout.LayoutParams params;
-    private View selectedView;
+    @InjectView(R.id.exp_value) TextView expValue;
+    @InjectView(R.id.exp_mask) ImageView expBarMask;
+    @InjectView(R.id.hp_value) TextView hpValue;
+    @InjectView(R.id.hp_mask) ImageView hpBarMask;
+    @InjectView(R.id.tasks_pager) ViewPager viewPager;
+    @InjectView(R.id.level) TextView level;
+    @InjectView(R.id.next_lvl) TextView nextLevel;
+    @InjectView(R.id.gold) TextView gold;
+    
     private int displayWidth;
-    private TextView level;
-    private TextView nextLevel;
-    private TextView gold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
         AuthProviderFactory.onCreate(this);
-        
         UserEventsProducer.produceGetUserStatsEvent(this);
         
         initUI();
@@ -61,51 +53,37 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
     private void initUI() {
         displayWidth = getResources().getDisplayMetrics().widthPixels;
         
-        gold = (TextView) findViewById(R.id.gold);
-        
-        viewPager = (ViewPager) findViewById(R.id.tasks_pager);
         viewPager.setAdapter(new MainScreenFragmentsAdapter(getSupportFragmentManager()));
         viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
-        
-        logout = (Button) findViewById(R.id.logout);
-        logout.setOnClickListener(this);
-
-        level = (TextView) findViewById(R.id.level);
-        nextLevel = (TextView) findViewById(R.id.next_lvl);
-        
-        expValue = (TextView) findViewById(R.id.exp_value);
-        expBarMask = (ImageView) findViewById(R.id.exp_mask);
-        
-        hpValue = (TextView) findViewById(R.id.hp_value);
-        hpBarMask = (ImageView) findViewById(R.id.hp_mask);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.logout:
-                AuthProviderFactory.getProvider().logout();
-                Intent intent = new Intent(MainActivity.this, AuthActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                break;
-        }
-    }
-    
     @Subscribe
     public void onUserStatsObtained(GetUserStatsEvent event) {
-        gold.setText(String.valueOf(event.getStats().getGold()));
-        
-        level.setText(String.valueOf(event.getStats().getLvl()));
-        nextLevel.setText(String.valueOf(event.getStats().getLvl() + 1));
-
-        expValue.setText(event.getStats().getExp() + " / " + event.getStats().getMaxLvlExp());
-        expBarMask.getLayoutParams().width = getViewWidth(getExpBarWidth(), event.getStats().getExp(), event.getStats().getMinLvlExp(),
-                event.getStats().getMaxLvlExp());
-
-        hpValue.setText(event.getStats().getHp() + " / 100");
-        hpBarMask.getLayoutParams().width = getViewWidth(displayWidth, event.getStats().getHp(), 0, 100);
+        updateGold(event.getStats().getGold());
+        updateLevels(event.getStats().getLvl());
+        updateExp(event.getStats().getExp(), event.getStats().getMinLvlExp(), event.getStats().getMaxLvlExp());
+        updateHP(event.getStats().getHp());
     }
+    
+    private void updateGold(int goldValue) {
+        gold.setText(String.valueOf(goldValue));
+    }
+    
+    private void updateLevels(int lvl) {
+        level.setText(String.valueOf(lvl));
+        nextLevel.setText(String.valueOf(lvl + 1));
+    }
+    
+    private void updateHP(int hp) {
+        hpValue.setText(hp + " / 100");
+        hpBarMask.getLayoutParams().width = getViewWidth(displayWidth, hp, 0, 100);
+    }
+    
+    private void updateExp(int exp, int minLvlExp, int maxLvlExp) {
+        expValue.setText(exp + " / " + maxLvlExp);
+        expBarMask.getLayoutParams().width = getViewWidth(getExpBarWidth(), exp, minLvlExp, maxLvlExp);
+    }
+    
     
     private int getViewWidth(int width, int value, int minValue, int maxValue) {
         return (value - minValue) * width / (maxValue - minValue);
@@ -118,14 +96,12 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
     @Override
     protected void onResume() {
         super.onResume();
-        
         BusProvider.getInstance().register(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        
         BusProvider.getInstance().unregister(this);
     }
 }
